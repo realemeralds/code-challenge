27c26d9a80560c712d150dc18ef64696446c0674
realemeralds <mattechane@gmail.com>
2022-08-02 02:38:59 +0800

    :sparkles: working on problem 2

---
+++ src/problem2/script.js
@@ -0,0 +1,6 @@
+inputAddress = document.querySelector("#input-address");
+inputAmount = document.querySelector("#input-amount");
+inputOTP = document.querySelector("#input-otp");
+
+generateOTPButton = document.querySelector("#otp-label");
+tokenSubmit = document.querySelector("#token-submit");
