4f42829030ec8020f31af87b9bd86bc465b1608d
realemeralds <102229990+realemeralds@users.noreply.github.com>
2022-08-07 21:25:16 +0800

    :sparkles: finished problem 5

--- src/problem4/retrieve-holders.ts
+++ src/problem4/retrieve-holders.ts
@@ -3,6 +3,7 @@ import { ethers } from "ethers";
 const provider = new ethers.providers.JsonRpcProvider(
   "https://bsc-dataseed.binance.org/"
 );
+
 const swthAddress: string = "0x250b211ee44459dad5cd3bca803dd6a7ecb5d46c";
 const swthAbi: string[] = [
   "function balanceOf(address) view returns (uint256)",
@@ -14,6 +15,7 @@ const wallets: string[] = [
   "0x0020c5222a24e4a96b720c06b803fb8d34adc0af",
   "0xfe808b079187cc460f47374580f5fb47c82b87a5",
 ];
+
 
 interface BigNumber {
   _hex: string;
